e4e76be493e86735a0b6ea63546eeee150c1fabb
Marcelo Sant'ana dos Anjos <104635591+AnjosSant@users.noreply.github.com>
2024-07-05 16:04:08 -0300

    Adicionando o gemini

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from PIL import Image, ImageTk
 import requests
 import os
 from dotenv import load_dotenv
+import google.generativeai as genai
 
 # Carregar variáveis de ambiente do arquivo .env
 load_dotenv()
@@ -18,6 +19,9 @@ PUBLISHED_NAME = os.getenv('PUBLISHED_NAME')
 SECOND_PROJECT_ID = os.getenv('SECOND_PROJECT_ID')
 SECOND_PUBLISHED_NAME = os.getenv('SECOND_PUBLISHED_NAME')
 
+# Configurar a chave da API do Gemini
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
 class HandDetectionApp:
     def __init__(self, root):
         """
@@ -28,6 +32,7 @@ class HandDetectionApp:
         """
         self.root = root
         self.root.title("Detecção de Mãos")
+        self.configure_generative_model()
 
         self.video_source = 0  # ID da câmera (0 para câmera padrão)
         self.vid = cv2.VideoCapture(self.video_source, cv2.CAP_DSHOW)
@@ -156,16 +161,12 @@ class HandDetectionApp:
         self.chat_log = tk.Text(chat_window, state='disabled', width=50, height=20)
         self.chat_log.grid(row=0, column=0, padx=10, pady=10)
 
-        self.entry_message = tk.Entry(chat_window, width=50)
-        self.entry_message.grid(row=1, column=0, padx=10, pady=10)
-        self.entry_message.bind("<Return>", self.send_message)
-
         self.chat_canvas = tk.Canvas(chat_window, width=self.vid.get(cv2.CAP_PROP_FRAME_WIDTH),
                                      height=self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        self.chat_canvas.grid(row=0, column=1, rowspan=2, padx=10, pady=10)
+        self.chat_canvas.grid(row=0, column=1, padx=10, pady=10)
 
         self.btn_send_signal = tk.Button(chat_window, text="Enviar Sinal de Libras", command=self.detect_libras)
-        self.btn_send_signal.grid(row=2, column=1, pady=10)
+        self.btn_send_signal.grid(row=1, column=1, pady=10)
 
         self.chat_vid = cv2.VideoCapture(self.video_source, cv2.CAP_DSHOW)
 
@@ -218,57 +219,58 @@ class HandDetectionApp:
             max_tag = None
 
             for item in predictions:
-                if item['tagName'] == 'Oi' and item['probability'] > max_probability:
+                if item['probability'] > max_probability and item['tagName'] != 'Not':
                     max_probability = item['probability']
                     max_tag = item['tagName']
+                    print(f"Tag com maior probabilidade: {max_tag}")
 
             if max_probability >= 0.1:
-                self.update_chat_log(f"Sinal detectado: {max_tag}")
-                self.generate_response("Oi, tudo bem? Como você está?")
-            else:
-                self.update_chat_log("Não entendi, faça novamente")
+                self.generate_response(max_tag)
+                print(f"Tag selecionada: {max_tag}")
+
+    def configure_generative_model(self):
+        """
+        Configura o modelo generativo para a geração de respostas.
+        """
+        self.generative_model = genai.GenerativeModel('gemini-1.5-flash')
+
+    def generate_response(self, max_tag):
+        """
+        Gera uma resposta com base na tag máxima detectada.
+
+        Args:
+            max_tag (str): Tag com maior probabilidade detectada.
+        """
+        context = f"Você está participando de uma conversa. e alguem vai falar com voce"
+        msg = f" {context} {max_tag}."
+        print(f"Prompt: {msg}")
+        try:
+            # Geração de resposta usando a API do Google Generative AI
+            chat_session = self.generative_model.start_chat(history=[]) 
+            response = chat_session.send_message(msg)
+            self.update_chat_log(response.text)
+        except Exception as e:
+            print(f"Erro ao gerar resposta: {e}")
 
     def update_chat_log(self, message):
         """
-        Atualiza o log de chat na interface gráfica.
+        Adiciona uma mensagem ao log de chat na interface gráfica.
 
         Args:
             message (str): Mensagem a ser adicionada ao log de chat.
         """
-        self.chat_log.config(state='normal')
+        self.chat_log.configure(state='normal')
         self.chat_log.insert(tk.END, f"{message}\n")
-        self.chat_log.config(state='disabled')
+        self.chat_log.configure(state='disabled')
         self.chat_log.see(tk.END)
-
-    def generate_response(self, user_input):
-        """
-        Gera uma resposta para exibir no log de chat.
-
-        Args:
-            user_input (str): Entrada do usuário para gerar a resposta.
-        """
-        # Aqui você pode modificar para gerar respostas baseadas em regras ou lógica programada
-        self.update_chat_log(f"IA: {user_input}")
-
-    def send_message(self, event):
-        """
-        Envia a mensagem digitada pelo usuário para o log de chat.
-
-        Args:
-            event (tk.Event): Evento de pressionamento de tecla.
-        """
-        message = self.entry_message.get()
-        self.entry_message.delete(0, tk.END)
-        self.update_chat_log(f"Você: {message}")
-        self.generate_response(message)
 
     def on_closing(self):
         """
-        Finaliza a aplicação ao fechar a janela principal.
+        Ação executada ao fechar a janela principal.
         """
         if self.vid.isOpened():
             self.vid.release()
-        if hasattr(self, 'chat_vid') and self.chat_vid.isOpened():
+        if self.chat_vid.isOpened():
             self.chat_vid.release()
         self.root.destroy()
 
